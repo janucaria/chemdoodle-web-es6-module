cd541d68197adf1e7359f51cb6d7b2f556222cde
Januar Andaria <janucaria@gmail.com>
2019-04-28 12:15:10 +0800

    Extract Bond class in structures package

--- src/ChemDoodle/structures/index.js
+++ src/ChemDoodle/structures/index.js
@@ -2,3 +2,4 @@ export { default as Point } from './Point';
 export { default as Queue } from './Queue';
 export { default as Query } from './Query';
 export { default as Ring } from './Ring';
+export { default as Bond } from './Bond';
