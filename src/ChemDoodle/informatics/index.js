e67177d1ea2f60aa912a73779be3802ef1540bf6
Januar Andaria <janucaria@gmail.com>
2019-04-28 16:23:13 +0800

    Extract EulerFacetRingFinder class in informatics package

--- src/ChemDoodle/informatics/index.js
+++ src/ChemDoodle/informatics/index.js
@@ -2,3 +2,4 @@ export { default as _Counter } from './_Counter';
 export { default as NumberOfMoleculesCounter } from './NumberOfMoleculesCounter';
 export { default as FrerejacqueNumberCounter } from './FrerejacqueNumberCounter';
 export { default as _RingFinder } from './_RingFinder';
+export { default as EulerFacetRingFinder } from './EulerFacetRingFinder';
