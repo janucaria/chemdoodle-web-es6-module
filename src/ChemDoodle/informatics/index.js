2885ffa3b4cbc7506691ea10f3b03737af751d9d
Januar Andaria <janucaria@gmail.com>
2019-04-28 16:32:20 +0800

    Extract SSSRFinder class in informatics package

--- src/ChemDoodle/informatics/index.js
+++ src/ChemDoodle/informatics/index.js
@@ -3,3 +3,4 @@ export { default as NumberOfMoleculesCounter } from './NumberOfMoleculesCounter'
 export { default as FrerejacqueNumberCounter } from './FrerejacqueNumberCounter';
 export { default as _RingFinder } from './_RingFinder';
 export { default as EulerFacetRingFinder } from './EulerFacetRingFinder';
+export { default as SSSRFinder } from './SSSRFinder';
--- src/ChemDoodleWeb-unpacked.js
+++ src/ChemDoodleWeb-unpacked.js
@@ -5934,41 +5934,6 @@ ChemDoodle.featureDetection = featureDetection;
 
 })(ChemDoodle.informatics, ChemDoodle.io, ChemDoodle.structures);
 
-(function(informatics, undefined) {
-	'use strict';
-	informatics.SSSRFinder = function(molecule) {
-		this.rings = [];
-		if (molecule.atoms.length > 0) {
-			var frerejacqueNumber = new informatics.FrerejacqueNumberCounter(molecule).value;
-			var all = new informatics.EulerFacetRingFinder(molecule).rings;
-			all.sort(function(a, b) {
-				return a.atoms.length - b.atoms.length;
-			});
-			for ( var i = 0, ii = molecule.bonds.length; i < ii; i++) {
-				molecule.bonds[i].visited = false;
-			}
-			for ( var i = 0, ii = all.length; i < ii; i++) {
-				var use = false;
-				for ( var j = 0, jj = all[i].bonds.length; j < jj; j++) {
-					if (!all[i].bonds[j].visited) {
-						use = true;
-						break;
-					}
-				}
-				if (use) {
-					for ( var j = 0, jj = all[i].bonds.length; j < jj; j++) {
-						all[i].bonds[j].visited = true;
-					}
-					this.rings.push(all[i]);
-				}
-				if (this.rings.length === frerejacqueNumber) {
-					break;
-				}
-			}
-		}
-	};
-
-})(ChemDoodle.informatics);
 (function(io, undefined) {
 	'use strict';
 	io._Interpreter = function() {
