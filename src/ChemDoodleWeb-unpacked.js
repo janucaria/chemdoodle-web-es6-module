323a28b1bd2359273030b3090cf857a9057ac805
Januar Andaria <janucaria@gmail.com>
2019-04-29 10:08:58 +0800

    Extract _SpectrumCanvas class in ChemDoodle package

--- src/ChemDoodleWeb-unpacked.js
+++ src/ChemDoodleWeb-unpacked.js
@@ -1540,42 +1540,6 @@ ChemDoodle.featureDetection = featureDetection;
 
 ChemDoodle.monitor = monitor;
 
-(function(c, document, undefined) {
-	'use strict';
-	c._SpectrumCanvas = function(id, width, height) {
-		if (id) {
-			this.create(id, width, height);
-		}
-	};
-	var _ = c._SpectrumCanvas.prototype = new c._Canvas();
-	_.spectrum = undefined;
-	_.emptyMessage = 'No Spectrum Loaded or Recognized';
-	_.loadMolecule = undefined;
-	_.getMolecule = undefined;
-	_.innerRepaint = function(ctx) {
-		if (this.spectrum && this.spectrum.data.length > 0) {
-			this.spectrum.draw(ctx, this.specs, this.width, this.height);
-		} else if (this.emptyMessage) {
-			ctx.fillStyle = '#737683';
-			ctx.textAlign = 'center';
-			ctx.textBaseline = 'middle';
-			ctx.font = '18px Helvetica, Verdana, Arial, Sans-serif';
-			ctx.fillText(this.emptyMessage, this.width / 2, this.height / 2);
-		}
-	};
-	_.loadSpectrum = function(spectrum) {
-		this.spectrum = spectrum;
-		this.repaint();
-	};
-	_.getSpectrum = function() {
-		return this.spectrum;
-	};
-	_.getSpectrumCoordinates = function(x, y) {
-		return spectrum.getInternalCoordinates(x, y, this.width, this.height);
-	};
-
-})(ChemDoodle, document);
-
 (function(c, undefined) {
 	'use strict';
 	c.ObserverCanvas = function(id, width, height) {
